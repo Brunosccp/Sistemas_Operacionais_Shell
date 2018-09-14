f821fb68d5f4d7d893c19c61aecec36798f5f16d
Brunosccp <rocca_bruno@hotmail.com>
2018-09-14 20:20:47 -0300

    adicionando mkdir e clear

--- shell_builtIn.h
+++ shell_builtIn.h
@@ -1,16 +1,22 @@
 int shell_cd(char **args);
+int shell_mkdir(char **args);
 int shell_help(char **args);
+int shell_clear(char **args);
 int shell_exit(char **args);
 
 char *builtIn_str[] = {
     "cd",
+    "mkdir",
     "help",
+    "clear",
     "exit"
 };
 
 int (*builtIn_func[]) (char**) = {
     &shell_cd,
+    &shell_mkdir,
     &shell_help,
+    &shell_clear,
     &shell_exit
 };
 
@@ -29,6 +35,18 @@ int shell_cd(char **args){
     }
     return 1;
 }
+int shell_mkdir(char **args){
+    if(args[1] == NULL){
+        fprintf(stderr, "SHELL ERROR: expected name for mkdir\n");
+    }else{
+        char path[50] = "./";
+        strcat(path, args[1]);
+
+        mkdir(path, 0777);
+    }
+
+    return 1;
+}
 
 int shell_help(char **args){
     int i;
@@ -39,6 +57,13 @@ int shell_help(char **args){
     for(i=0; i < shell_num_bultIns(); i++){
         printf("    %s\n", builtIn_str[i]);
     }
+
+    printf("\nReference: brennan.io/blog\n");
+
+    return 1;
+}
+int shell_clear(char **args){
+    printf("\033[H\033[J");
 
     return 1;
 }
